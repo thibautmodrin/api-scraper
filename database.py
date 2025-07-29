ac9b390209c1f9256408a0ff41039a7979bc3290
thibautmodrin <tibomodrin@gmail.com>
2025-07-29 15:23:26 +0200

    add database.py

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import os
 import psycopg2
+from datetime import date
+
 
 def get_db_connection():
     return psycopg2.connect(
@@ -9,3 +11,27 @@ def get_db_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
     )
+
+
+def has_already_scraped_today(keyword: str) -> bool:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT 1 FROM scraping_logs WHERE keyword = %s AND date = CURRENT_DATE",
+        (keyword,)
+    )
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result is not None
+
+def log_scraping(keyword: str):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO scraping_logs (keyword, date) VALUES (%s, %s)",
+        (keyword, date.today())
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
